b449857bf792291012f64a0092f0f4947fa7879a
melonicedlatte <melonicedlatte@gmail.com>
2019-10-05 20:32:24 +0900

    - make image color randomly

--- utils/show_bbox.py
+++ utils/show_bbox.py
@@ -2,8 +2,9 @@ import PIL.Image as Image
 import PIL.ImageFont as ImageFont
 import PIL.ImageDraw as ImageDraw
 import numpy as np
+from random import randint
 
-def draw_bounding_box_on_image(image, box, display_class, color='red', thickness=4, is_normalized=True):
+def draw_bounding_box_on_image(image, box, display_class, color, thickness=4, is_normalized=True):
 	"""
 
 	Args:
@@ -25,13 +26,15 @@ def draw_bounding_box_on_image(image, box, display_class, color='red', thickness
 		(left, right, top, bottom) = (xmin * im_width, xmax * im_width, ymin * im_height, ymax * im_height)
 	else:
 		(left, right, top, bottom) = (xmin, xmax, ymin, ymax)
-	draw.line([(left, top), (left, bottom), (right, bottom),
-             (right, top), (left, top)], width=thickness, fill=color)
+
+	draw.line([(left, top), (left, bottom), (right, bottom), (right, top), (left, top)], width=thickness, fill=color)
+
+	print((left, top), (left, bottom), (right, bottom), (right, top), (left, top), color)
 
 	# font size
 	#font = ImageFont.load_default()
 	img_fraction = 0.05
-	font_path = '/Library/Fonts/Arial.ttf'
+	font_path = 'BMJUA_ttf.ttf'
 	font = ImageFont.truetype(font_path, 24)
 	#font = ImageFont.truetype(fm.findfont(fm.FontProperties(family=combo.get())), 24)
 	fontsize = 1
@@ -50,18 +53,35 @@ def draw_bounding_box_on_image(image, box, display_class, color='red', thickness
 	image.save('result_image.png')
 
 
-def draw_bounding_boxes_on_image(image, boxes, display_classes=[], color='red', thickness=4, is_normalized=True):
+def draw_bounding_boxes_on_image(image, boxes, display_classes=[], color=[], thickness=4, is_normalized=True):
 	"""
 
 	Args:
 		image:
 		boxes:
-		color:
+		color: 
 		display_classes:
 		thickness:
 		is_normalized:
 	"""
 # image, box, color='red', display_class, thickness=4, is_normalized=True)
-	for box, _class in zip(boxes, display_classes):
-		draw_bounding_box_on_image(image, box, _class, color, thickness, is_normalized)
 
+	# If user not give color array, automatically make
+	if color == []:
+		color_dic = {}
+		for _class in display_classes:
+			if _class not in color_dic.keys():
+				color_dic[_class] = make_color_random()
+			color.append(color_dic[_class])
+
+	
+	for box, _color, _class in zip(boxes, color, display_classes):
+		print(box, _color, _class)
+		# draw_bounding_box_on_image(image, box, _class, color, thickness, is_normalized)
+		draw_bounding_box_on_image(image, box, _class, _color, thickness, is_normalized)
+
+def make_color_random():
+	r = randint(0,255)
+	g = randint(0,255)
+	b = randint(0,255)
+	return (r, g, b)
--- utils/test.py
+++ utils/test.py
@@ -2,4 +2,6 @@ import show_bbox
 from PIL import Image
 
 img = Image.open('image1.png')
-show_bbox.draw_bounding_boxes_on_image(img, [(0.2, 0.2, 0.5, 0.5)], display_classes=['twice'])
+# img = Image.open('image2.jpeg')
+# show_bbox.draw_bounding_boxes_on_image(img, [(0.2, 0.2, 0.5, 0.5), (0.1, 0.1, 0.3, 0.3), (0.5, 0.5, 0.8, 0.8)], display_classes=['twice', 'nayeon', 'twice'])
+show_bbox.draw_bounding_boxes_on_image(img, [(0, 0, 100, 100), (50, 50, 150, 150), (200, 200, 300, 300)], display_classes=['twice', 'nayeon', 'twice'], is_normalized=False)
