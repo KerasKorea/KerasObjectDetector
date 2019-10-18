338358e58bd5c0297634c00160eca6cf84e2d067
visionNoob <insurgent92@gmail.com>
2019-10-18 14:20:56 +0900

    Move Branch (#37)

--- CONTRIBUTING.md
+++
@@ -1,1 +0,0 @@
-# see https://github.com/keras-team/keras
--- setup.py
+++ setup.py
@@ -1,48 +1,67 @@
-from setuptools import setup
-from setuptools import find_packages
+import setuptools
+from setuptools.extension import Extension
+from distutils.command.build_ext import build_ext as DistUtilsBuildExt
 
-long_description = '''
-Keras Object Detector is a high-level object detection API,
-written in Python and capable of running on top of
-TensorFlow, CNTK, or Theano.
 
-Use Keras Object Detector if you need a object detecting library that:
+class BuildExtension(setuptools.Command):
+    description     = DistUtilsBuildExt.description
+    user_options    = DistUtilsBuildExt.user_options
+    boolean_options = DistUtilsBuildExt.boolean_options
+    help_options    = DistUtilsBuildExt.help_options
 
-- Allows for easy and fast prototyping
-  (through user friendliness, modularity, and extensibility).
-- Runs seamlessly on CPU and GPU.
+    def __init__(self, *args, **kwargs):
+        from setuptools.command.build_ext import build_ext as SetupToolsBuildExt
 
-Read the documentation at: # todo: keras object detector tutorial, quick-start
+        # Bypass __setatrr__ to avoid infinite recursion.
+        self.__dict__['_command'] = SetupToolsBuildExt(*args, **kwargs)
 
-For a detailed overview of what makes Keras special, see:
-https://keras.io/why-use-keras/
-'''
-setup(name='KerasObjectDetector',
-      version='0.0.1',
-      description='Object Detector for Keras User',
-      long_description=long_description,
-      author='Keras Korea',
-      author_email='',
-      url='https://github.com/KerasKorea/KerasObjectDetector',
-      download_url='https://github.com/keras-team/keras/tarball/2.3.0',
-      license='MIT or Apache',
-      install_requires=['numpy>=1.9.1',
-                        'scipy>=0.14',
-                        'six>=1.9.0',
-                        'pyyaml',
-                        'h5py',
-                        'six',
-                        'tensorflow'],
-      extras_require={
-          'visualize': ['pydot>=1.2.4', 'matplotlib'],
-          'tests': ['pytest',
-                    'pytest-pep8',
-                    'pytest-xdist',
-                    'flaky',
-                    'pytest-cov',
-                    'pandas',
-                    'requests',
-                    'markdown'],
-          'image': ['pillow']
-      },
-      packages=find_packages())
+    def __getattr__(self, name):
+        return getattr(self._command, name)
+
+    def __setattr__(self, name, value):
+        setattr(self._command, name, value)
+
+    def initialize_options(self, *args, **kwargs):
+        return self._command.initialize_options(*args, **kwargs)
+
+    def finalize_options(self, *args, **kwargs):
+        ret = self._command.finalize_options(*args, **kwargs)
+        import numpy
+        self.include_dirs.append(numpy.get_include())
+        return ret
+
+    def run(self, *args, **kwargs):
+        return self._command.run(*args, **kwargs)
+
+
+extensions = [
+    Extension(
+        'keras_retinanet.utils.compute_overlap',
+        ['keras_retinanet/utils/compute_overlap.pyx']
+    ),
+]
+
+
+setuptools.setup(
+    name             = 'keras-retinanet',
+    version          = '0.5.1',
+    description      = 'Keras implementation of RetinaNet object detection.',
+    url              = 'https://github.com/fizyr/keras-retinanet',
+    author           = 'Hans Gaiser',
+    author_email     = 'h.gaiser@fizyr.com',
+    maintainer       = 'Hans Gaiser',
+    maintainer_email = 'h.gaiser@fizyr.com',
+    cmdclass         = {'build_ext': BuildExtension},
+    packages         = setuptools.find_packages(),
+    install_requires = ['keras', 'keras-resnet==0.1.0', 'six', 'scipy', 'cython', 'Pillow', 'opencv-python', 'progressbar2'],
+    entry_points     = {
+        'console_scripts': [
+            'retinanet-train=keras_retinanet.bin.train:main',
+            'retinanet-evaluate=keras_retinanet.bin.evaluate:main',
+            'retinanet-debug=keras_retinanet.bin.debug:main',
+            'retinanet-convert-model=keras_retinanet.bin.convert_model:main',
+        ],
+    },
+    ext_modules    = extensions,
+    setup_requires = ["cython>=0.28", "numpy>=1.14.0"]
+)
