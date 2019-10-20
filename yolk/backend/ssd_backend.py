dc55ff435d802851776fb26c409bf7e027f4e800
Sukjoo Hong <hngskj@Sukjoos-MacBook-Pro.local>
2019-10-20 13:09:51 +0900

    Add ssd_backend

--- yolk/backend/ssd_backend.py
+++ yolk/backend/ssd_backend.py
@@ -1,1 +1,16 @@
-# TODO:
+import keras_ssd
+from keras import models
+
+from keras_ssd.keras_loss_function.keras_ssd_loss import SSDLoss
+from keras_ssd.keras_layers.keras_layer_AnchorBoxes import AnchorBoxes
+from keras_ssd.keras_layers.keras_layer_DecodeDetections import DecodeDetections
+from keras_ssd.keras_layers.keras_layer_L2Normalization import L2Normalization
+
+def load_model(model_path):
+    ssd_loss = SSDLoss(neg_pos_ratio=3, n_neg_min=0, alpha=1.0)
+    model = models.load_model(model_path, custom_objects={'AnchorBoxes': AnchorBoxes,
+                                                          'L2Normalization': L2Normalization,
+                                                          'DecodeDetections': DecodeDetections,
+                                                          'compute_loss': ssd_loss.compute_loss})
+    return model
+    
