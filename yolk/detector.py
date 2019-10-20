7012b8ba88c9d5d3777ff2ca2c6d24125bd7162f
Sukjoo Hong <hngskj@Sukjoos-MacBook-Pro.local>
2019-10-20 18:27:05 +0900

    Add backend methods

--- yolk/detector.py
+++ yolk/detector.py
@@ -1,7 +1,20 @@
 from . import backend as M
 
-def load_model(path, backbone='resnet50'):
-    return M.load_model(path, backbone)
+def load_inference_model(path, *args):
+    return M.load_inference_model(path, *args)
 
-def preprocessing_image(image):
-    return M.preprocess_image(image)
+def load_training_model(num_classes, *args):
+    return M.load_training_model(num_classes, *args)
+
+def preprocessing_image(image, *args):
+    return M.preprocessing_image(image, *args)
+
+def get_train_loader(args):
+    return 1#train_generator, validation_generator = M.create_generators()
+
+def prostprocessing_image(model_output, args):
+    #bbox ,score ,~  = .prostprocessing_image(model_output)
+    return 1#bbox ,score ,~ 
+
+def get_loss():
+    return M.get_loss()
